a5199a256d4e5da7fa667beda6228e39a6067df5
JuyelRana <mjuyelrana@gmail.com>
2021-07-26 21:54:04 +0600

    Formated coding structure

--- itclan-assignment2/routes/web.php
+++ itclan-assignment2/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+
+    return Auth::check() ? view('home') : redirect()->route('login');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
