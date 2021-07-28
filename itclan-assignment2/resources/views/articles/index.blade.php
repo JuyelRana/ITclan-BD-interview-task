de09c0150b1cd386a2508ca07f9db1029c3c9229
JuyelRana <mjuyelrana@gmail.com>
2021-07-28 16:06:16 +0600

    news api integrated

--- itclan-assignment2/resources/views/articles/index.blade.php
+++ itclan-assignment2/resources/views/articles/index.blade.php
@@ -50,7 +50,7 @@
     <script>
 
         setTimeout(() => {
-            $.get('articles_url', function (response, status) {
+            $.get('https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=0b4c3b2699bf43ccbb8f807f7f6ef24d', function (response, status) {
                 if (response.status === 'ok') {
                     let tableRows = response.articles.map((article, index) => {
                         return "<tr>"
